2b31eea8cb79bc62098980f74b4801ef99763832
Vedaant <vedaant.varshney@gmail.com>
2021-09-18 10:46:02 -0400

    Vedaant Folder

---
+++ git.sh
@@ -0,0 +1,7 @@
+echo "# BeatBlocks" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Deep-Thoughts42/BeatBlocks.git
+git push -u origin main
