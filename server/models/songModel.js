c0edd9f7c05fdc9f45387b0b32d53cb2e0aa410a
Robert Muresan <robert.victor.muresan@gmail.com>
2021-09-18 21:48:42 -0400

    name

--- server/models/songModel.js
+++ server/models/songModel.js
@@ -1,8 +1,22 @@
 const { boolean } = require("hardhat/internal/core/params/argumentTypes");
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require('uuid');
+var generate = require('project-name-generator');
+
+function titleCase() {
+  var str = generate({words: 3}).spaced
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  return splitStr.join(' '); 
+}
 
 const SongSchema = new mongoose.Schema({
+  songName: {
+    type: String,
+    default: titleCase,
+  },
   parts: {
     type: [{
       audio: {type: String},
